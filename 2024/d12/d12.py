8bc85f3bf1761b23e151fce8624e56bc0b404c1a
James Tan <james_tan97@outlook.com>
2024-12-12 19:31:07 -0500

    add 2024 d12 python solution

--- 2024/d12/d12.py
+++ 2024/d12/d12.py
@@ -1,40 +1,34 @@
 import sys
-from functools import lru_cache
+from collections import defaultdict
 
 file = open(sys.argv[1]).read()
 
 
 grid = [list(e) for e in file.strip().splitlines()]
-num_rows = len(grid)
-num_cols = len(grid[0])
+num_rows, num_cols = len(grid), len(grid[0])
 grid = {complex(i, j): v for i, row in enumerate(grid) for j, v in enumerate(row)}
-
-groups = []
-
-to_visit = set(grid.keys())
 
 
 def in_grid(coords):
     return 0 <= coords.real < num_rows and 0 <= coords.imag < num_cols
 
 
+groups = []
+to_visit = set(grid.keys())
+
 while to_visit:
-    print("new group")
     curr = to_visit.pop()
-    group = {curr}
+    group = []
     queue = {curr}
 
     while len(queue):
         curr = queue.pop()
-        print(curr, grid[curr])
-        group.add(curr)
+        group.append(curr)
         to_visit -= {curr}
         for delta in (1, -1, 1j, -1j):
             coords2 = curr + delta
-            if not in_grid(coords2):
-                continue
             if (
-                coords2 in to_visit
+                in_grid(coords2)
                 and coords2 not in group
                 and grid[coords2] == grid[curr]
             ):
@@ -42,19 +36,34 @@ while to_visit:
 
     groups.append(group)
 
-print(groups)
+part1 = 0
+part2 = 0
 
-total = 0
 for group in groups:
-    area =len(group)
-    perimeter = 0
+    area = len(group)
+    side_pieces = defaultdict(lambda: set())
     for curr in group:
         for delta in (1, -1, 1j, -1j):
             coords2 = curr + delta
             if grid.get(coords2) != grid[curr]:
-                perimeter += 1
-    print(grid[curr]) # type: ignore
-    print("area: ", len(group))
-    print("perimeter: ", perimeter)
-    total += area * perimeter
-print(total)
+                side_pieces[delta].add(curr + delta)
+
+    perimeter = sum(len(val) for val in side_pieces.values())
+
+    sides = 0
+    for side, dirs in [(1, [1j, -1j]), (-1, [1j, -1j]), (1j, [1, -1]), (-1j, [1, -1])]:
+        pieces = set(side_pieces[side])
+        while pieces:
+            sides += 1
+            piece = pieces.pop()
+
+            for d in dirs:
+                curr = piece
+                while (curr + d) in pieces:
+                    pieces.remove(curr + d)
+                    curr += d
+
+    part1 += area * perimeter
+    part2 += area * sides
+
+print(part1, part2)
