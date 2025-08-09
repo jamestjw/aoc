0d339483176089eba7ae2f4e768e678f5943ced6
James Tan <james_tan97@outlook.com>
2025-08-09 18:15:51 -0400

    2022 d19 python solution

--- 2022/d19/d19.py
+++ 2022/d19/d19.py
@@ -1,111 +1,121 @@
-"""
-This file contains a code attempt for advent of code day 19
-"""
+import sys
+import math
+import re
+from operator import add
+
+file = open(sys.argv[1]).read().strip()
+numbers = re.findall(r"\d+", file)
+# Always skip the first number and take the subsequent 6
+blueprints = [tuple(map(int, numbers[i : i + 6])) for i in range(1, len(numbers), 7)]
 
 
-import os
-from functools import lru_cache
+def run(
+    cache: dict,
+    inventory: tuple[int, int, int, int, int, int, int, int],
+    iters,
+    blueprint,
+):
+    ore, clay, obsidian, geode, ore_bot, clay_bot, obsidian_bot, geode_bot = inventory
+    (
+        ore_ore_cost,
+        clay_ore_cost,
+        obsi_ore_cost,
+        obsi_clay_cost,
+        geode_ore_cost,
+        geode_obsi_cost,
+    ) = blueprint
+
+    if iters == 0:
+        return cache, geode
+
+    if (score := cache.get((iters, inventory))) is not None:
+        return cache, score
+
+    null = inventory
+    buy_ore = (
+        ore - ore_ore_cost,
+        clay,
+        obsidian,
+        geode,
+        ore_bot + 1,
+        clay_bot,
+        obsidian_bot,
+        geode_bot,
+    )
+    buy_clay = (
+        ore - clay_ore_cost,
+        clay,
+        obsidian,
+        geode,
+        ore_bot,
+        clay_bot + 1,
+        obsidian_bot,
+        geode_bot,
+    )
+    buy_obs = (
+        ore - obsi_ore_cost,
+        clay - obsi_clay_cost,
+        obsidian,
+        geode,
+        ore_bot,
+        clay_bot,
+        obsidian_bot + 1,
+        geode_bot,
+    )
+    buy_geod = (
+        ore - geode_ore_cost,
+        clay,
+        obsidian - geode_obsi_cost,
+        geode,
+        ore_bot,
+        clay_bot,
+        obsidian_bot,
+        geode_bot + 1,
+    )
+
+    if ore >= geode_ore_cost and obsidian >= geode_obsi_cost:
+        next_inventories = [buy_geod]
+    elif ore >= obsi_ore_cost and clay >= obsi_clay_cost:
+        next_inventories = [buy_obs]
+    else:
+        next_inventories = [
+            i for i in [null, buy_clay, buy_ore] if all(e >= 0 for e in i)
+        ]
+
+    next_inventories = [
+        tuple(map(add, i, (ore_bot, clay_bot, obsidian_bot, geode_bot, 0, 0, 0, 0)))
+        for i in next_inventories
+    ]
+
+    cache2 = cache
+    scores = []
+    for inv in next_inventories:
+        cache2, score = run(cache2, inv, iters - 1, blueprint)
+        scores.append(score)
+    best_score = max(scores)
+    cache2[(iters, inventory)] = best_score
+
+    return cache2, best_score
 
 
-if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+print(
+    sum(
+        [
+            i * score
+            for i, (_, score) in enumerate(
+                [run(dict(), (0, 0, 0, 0, 1, 0, 0, 0), 24, bp) for bp in blueprints], 1
+            )
+        ]
+    )
+)
 
-    day = __file__.split("\\", maxsplit=-1)[-1][:-3]
-    my_tests = open(f"{day}_tests_input.txt", "r", encoding="utf-8").read().splitlines()
-
-    ore_robot_ore_cost = 4
-    clay_robot_ore_cost = 2
-    obsidian_robot_ore_cost = 3
-    obsidian_robot_clay_cost = 14
-    geode_robot_ore_cost = 2
-    geode_robot_obsidian_cost = 7
-
-    ore_robot_count = 1
-    clay_robot_count = 0
-    obsidian_robot_count = 0
-    geode_robot_count = 0
-
-    ores = 0
-    clays = 0
-    obsidians = 0
-    geodes = 0
-
-    @lru_cache(maxsize=1000000000)
-    def traverse(
-        i,
-        ores,
-        clays,
-        obsidians,
-        geodes,
-        ore_robots,
-        clay_robots,
-        obsidian_robots,
-        geode_robots,
-    ):
-        print(
-            ores,
-            clays,
-            obsidians,
-            geodes,
-            ore_robots,
-            clay_robots,
-            obsidian_robots,
-            geode_robots,
-        )
-        if i == 0:
-            return geodes
-        options = [geodes]
-        for num_ore_purchased in range(ores // ore_robot_ore_cost + 1):
-            remaining_ores = ores - num_ore_purchased * ore_robot_ore_cost
-            for num_clay_purchased in range(remaining_ores // clay_robot_ore_cost + 1):
-                remaining_ores2 = (
-                    remaining_ores - num_clay_purchased * clay_robot_ore_cost
-                )
-                for num_obsidian_purchased in range(
-                    min(
-                        remaining_ores2 // obsidian_robot_ore_cost,
-                        clays // obsidian_robot_clay_cost,
-                    )
-                    + 1
-                ):
-                    remaining_ores3 = (
-                        remaining_ores2
-                        - num_obsidian_purchased * obsidian_robot_ore_cost
-                    )
-                    remaining_clay = (
-                        clays - num_obsidian_purchased * obsidian_robot_clay_cost
-                    )
-                    for num_geode_purchased in range(
-                        min(
-                            remaining_ores3 // geode_robot_ore_cost,
-                            obsidians // geode_robot_obsidian_cost,
-                        )
-                        + 1
-                    ):
-                        remaining_ores4 = (
-                            remaining_ores3 - num_geode_purchased * geode_robot_count
-                        )
-                        remaining_obsidians = (
-                            obsidians - num_geode_purchased * geode_robot_obsidian_cost
-                        )
-                        options.append(
-                            traverse(
-                                i - 1,
-                                remaining_ores4 + ore_robots,
-                                remaining_clay + clay_robots,
-                                remaining_obsidians + obsidian_robots,
-                                geodes + geode_robots,
-                                ore_robots + num_ore_purchased,
-                                clay_robots + num_clay_purchased,
-                                obsidian_robots + num_obsidian_purchased,
-                                geode_robots + num_geode_purchased,
-                            )
-                        )
-
-        return max(options)
-
-    part1 = traverse(24, 0, 0, 0, 0, 1, 0, 0, 0)
-    part2 = 0
-
-    print(f"Answer part1 : {part1}")
-    print(f"Answer part2 : {part2}")
+print(
+    math.prod(
+        [
+            score
+            for (_, score) in [
+                run(dict(), (0, 0, 0, 0, 1, 0, 0, 0), 32, bp) for bp in blueprints[:3]
+            ]
+        ]
+    )
+)
